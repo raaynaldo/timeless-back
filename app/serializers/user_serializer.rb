c8a84abe6f7d5ffadea81214fbe6b240a3b59c69
raaynaldo <raaynaldo@hotmail.com>
2021-01-01 12:59:36 -0500

    update seeds file and serializer.

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,3 +1,3 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :username, :full_name, :followers, :first_name, :last_name
+  attributes :username, :full_name, :first_name, :last_name
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -11,9 +11,12 @@ Post.destroy_all
 Tag.destroy_all
 PostTag.destroy_all
 
-john = User.create(first_name: 'John', last_name: 'Doe')
-bryce = User.create(first_name: 'Bryce', last_name: 'Moore')
+john = User.create(first_name: 'John', last_name: 'Doe', username: "johnd", password: "123")
+bryce = User.create(first_name: 'Bryce', last_name: 'Moore', username: "brycem", password: "123")
+ray = User.create(first_name: 'Raynaldo', last_name: 'Sutisna', username: "raynaldos", password: "123")
 UserFollower.create(followee_id: john.id, follower_id: bryce.id)
+UserFollower.create(followee_id: john.id, follower_id: ray.id)
+UserFollower.create(followee_id: bryce.id, follower_id: ray.id)
 post1 = Post.create(user_id: bryce.id, body: "hello")
 tag1 = Tag.create(name: "cool")
 PostTag.create(post_id: post1.id, tag_id: tag1.id)
