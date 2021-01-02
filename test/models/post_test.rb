5476c7f412e8dc970bf010af3a6e886ded734f47
raaynaldo <raaynaldo@hotmail.com>
2021-01-02 18:26:12 -0500

    post and user unit test

--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -1,7 +1,26 @@
 require "test_helper"
+require "pry"
 
 class PostTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  def setup
+    # user = User.first_or_create(username: "raaynaldo", first_name: "Raynaldo", password: "123", last_name: "Sutisna", email: "me@raaynaldo.com", birthday: "2020-01-31")
+    @post = Post.new(body: "Win a lottery", image: "", post_date: nil, user_id: User.first.id)
+  end
+
+  test "post should be valid" do
+    assert @post.valid?
+  end
+
+  test "body should be present" do
+    @post.body = " "
+    assert_not @post.valid?
+  end
+
+  test "user_id should be present" do
+    @post.user_id = nil
+    assert_not @post.valid?
+  end
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -5,39 +5,44 @@ class UserTest < ActiveSupport::TestCase
   #   assert true
   # end
   def setup
-    @user = User.new(username: "raaynaldo", password: "123", first_name: "Raynaldo", last_name: "Sutisna", email: "me@raaynaldo.com", birthday: "2020-01-31")
+    @user = User.new(username: "raaynaldo", password: "123", first_name: "Raynaldo", last_name: "Sutisna", email: "raynaldo_sutisna@example.com", birthday: "2020-01-31")
+    john = User.create!(first_name: 'John', last_name: 'Doe', username: "johnd", email: "john_doe@example.com", password: "123", birthday: "1990-01-01")
+    bryce = User.create!(first_name: 'Bryce', last_name: 'Moore', username: "brycem", email: "bryce_moore@example.com", password: "123", birthday: "1991-01-01")
+    matteo = User.create!(first_name: 'Matteo', last_name: 'Ricci', username: "matteor", email: "matteo_ricci@example.com", password: "123", birthday: "1992-01-01")
+    @user.followers += [john, bryce, matteo]
   end
 
   test "user should be valid" do
+    byebug  
     assert @user.valid?
   end
 
-  test "username should be valid" do
+  test "username should be present" do
     @user.username = " "
     assert_not @user.valid?
   end
 
-  test "password should be valid" do
+  test "password should be present" do
     @user.password = " "
     assert_not @user.valid?
   end
 
-  test "first_name should be valid" do
+  test "first_name should be present" do
     @user.first_name = " "
     assert_not @user.valid?
   end
 
-  test "last_name should be valid" do
+  test "last_name should be present" do
     @user.last_name = " "
     assert_not @user.valid?
   end
 
-  test "email should be valid" do
+  test "email should be present" do
     @user.email = " "
     assert_not @user.valid?
   end
 
-  test "birthday should be valid" do
+  test "birthday should be present" do
     @user.birthday = nil
     assert_not @user.valid?
   end
