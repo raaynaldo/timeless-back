7e670d28aa53c8ef272e12dc3660644fdef98c1e
raaynaldo <raaynaldo@hotmail.com>
2021-01-05 21:30:35 -0500

    update post serializer

--- app/serializers/post_serializer.rb
+++ app/serializers/post_serializer.rb
@@ -1,4 +1,4 @@
 class PostSerializer < ActiveModel::Serializer
-  attributes :user_id, :body, :post_date
+  attributes :user_id, :body, :post_date, :image
   has_many :tags, serializer: TagSerializer
 end
