5531209b1f24d0e669cf1d34f0d21c2a4268aee8
brycemooore <brycemooore@gmail.com>
2021-01-07 12:51:08 -0500

    update feed

--- app/serializers/post_serializer.rb
+++ app/serializers/post_serializer.rb
@@ -1,4 +1,5 @@
 class PostSerializer < ActiveModel::Serializer
   attributes :user_id, :body, :post_date, :image
   has_many :tags, serializer: TagSerializer
+  belongs_to :user, serializer: UserSerializer
 end
